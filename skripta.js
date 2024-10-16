341f1686aed4e82aaa4b0f9748682dd1c3507f4b
Anže Šinigoj <68126135+AnzeSinigoj@users.noreply.github.com>
2024-10-16 17:29:23 +0200

    Tretji commit

--- skripta.js
+++ skripta.js
@@ -27,9 +27,9 @@ datVracKz = dat_vrac_kz
 kzV1 = nk1
 kzV2 = nk2
 kzV3 = nk3
-raz = razlog <- textarea!
 */
 
+//osnovno
 const ime = document.getElementById("ime");
 const priimek = document.getElementById("priimek");
 const davcna = document.getElementById("davcnaST");
@@ -38,23 +38,20 @@ const email = document.getElementById("mail");
 const ulica = document.getElementById("ulica_hisna_st");
 const mesto = document.getElementById("mesto");
 const posta = document.getElementById("postnaST");
-
-
-document.getElementById('dejanska_forma').addEventListener('submit', function(event) {
-    const ime_V = ime.value.trim();
-    const priimek_V = priimek.value.trim();
-    const davcna_V = davcna.value.trim();
-    const tel_V = tel.value.trim();
-    const ulica_V = ulica.value.trim();
-    const mesto_V = mesto.value.trim();
-    const posta_V= posta.value.trim();
-
-    if (!imeValue || !priimekValue || !davcnaValue || !ulicaValue || !mestoValue || !postaValue) {
-        event.preventDefault();
-        alert('Vse oznacena polja so obvezna!');
-        return;
-    }
-});
+//izdaja
+const datumIzpita = document.getElementById("dat");
+const kz1 = document.getElementById("k1");
+const kz2 = document.getElementById("k2");
+const kz3 = document.getElementById("k3");
+//podalsanje
+const kzPodal = document.getElementById("k_pod");
+const datumIzdaje = document.getElementById("dat_izdaje");
+//vracilo
+const kzVracila = document.getElementById("vrac_kz");
+const datVracKz = document.getElementById("dat_vrac_kz");
+const kzV1 = document.getElementById("nk1");
+const kzV2 = document.getElementById("nk2");
+const kzV3 = document.getElementById("nk3");
 
 //davcna (SI85218521)
 davcna.addEventListener("input", function (event) {
@@ -62,10 +59,10 @@ davcna.addEventListener("input", function (event) {
     let vr = davcna.value.replace(/\D/g, ''); // \D vse kar ni stevilo odreze, velja skozi celotn string -> g, / -zacetek regex / -konec regex
 
     if (vr.length > 8) {
-        vr = vr.slice(0, 8); 
+        vr = vr.slice(0, 8);
     }
 
-    davcna.value = 'SI' + vr; 
+    davcna.value = 'SI' + vr;
 });
 
 //telefon
@@ -83,19 +80,34 @@ tel.addEventListener("input", function (event) {
 });
 
 //postna st.
-posta.addEventListener("input", function(event){
+posta.addEventListener("input", function (event) {
 
-    let vr = posta.value.replace(/\D/g,'');
+    let vr = posta.value.replace(/\D/g, '');
 
-    if(vr.length > 4){
-        vr = vr.slice(0,4);
+    if (vr.length > 4) {
+        vr = vr.slice(0, 4);
     }
 
     posta.value = vr;
 });
 
-//Za onemogocat pisat use 3 (ko kliknes naredi sivo)
+//vnosna maska za klicne znake:
 
+//izdaja
+kz1.addEventListener("input", maskaKlicniZnak);
+kz2.addEventListener("input", maskaKlicniZnak);
+kz3.addEventListener("input", maskaKlicniZnak);
+
+//podalsanje
+kzPodal.addEventListener("input", maskaKlicniZnak);
+
+//vracilo
+datVracKz.addEventListener("input", maskaKlicniZnak);
+kzV1.addEventListener("input", maskaKlicniZnak);
+kzV2.addEventListener("input", maskaKlicniZnak);
+kzV3.addEventListener("input", maskaKlicniZnak);
+
+//Za onemogocat pisat use 3 (ko kliknes naredi sivo):
 //POMEMBNO:
 let dolocilo_input = 0;
 //Ta spremenljivka odloca kateri input se uzame ko se to procesira 1 = izd 2 = pod 3 = vrac 0 = ni valid
@@ -103,7 +115,7 @@ const izd = document.getElementById('izdaja');
 const pod = document.getElementById('podalsanje');
 const vrac = document.getElementById('vracilo');
 
-izd.addEventListener('click', function() {
+izd.addEventListener('click', function () {
     izd.classList.remove('osivi');
     pod.classList.add('osivi');
     vrac.classList.add('osivi');
@@ -113,7 +125,7 @@ izd.addEventListener('click', function() {
 
     dolocilo_input = 1;
 });
-pod.addEventListener('click', function() {
+pod.addEventListener('click', function () {
     izd.classList.add('osivi');
     pod.classList.remove('osivi');
     vrac.classList.add('osivi');
@@ -123,7 +135,7 @@ pod.addEventListener('click', function() {
 
     dolocilo_input = 2;
 });
-vrac.addEventListener('click', function() {
+vrac.addEventListener('click', function () {
     izd.classList.add('osivi');
     pod.classList.add('osivi');
     vrac.classList.remove('osivi');
@@ -134,11 +146,60 @@ vrac.addEventListener('click', function() {
     dolocilo_input = 3;
 });
 
+//previri input ce je polno
+document.getElementById('dejanska_forma').addEventListener('submit', function (event) {
+    let el = [];
+
+    switch (dolocilo_input) {
+        case 1: // izdaja
+            el = [ime, priimek, davcna, ulica, mesto, posta, datumIzpita, kz1, kz2, kz3];
+            break;
+        case 2: // podal.
+            el = [ime, priimek, davcna, ulica, mesto, posta, kzPodal, datumIzdaje];
+            break;
+        case 3: // vrac.
+            el = [ime, priimek, davcna, ulica, mesto, posta, kzVracila, datVracKz, kzV1, kzV2, kzV3];
+            break;
+        default:
+            event.preventDefault();
+            alert('Prosim izberite eno izmed moznosti!');
+            return;
+    }
+
+    preveriVnose(el, event);
+})
+
 function pocistiVnose(div) {
     const vn = div.querySelectorAll('input');
 
     for (let i = 0; i < vn.length; i++) {
         vn[i].value = '';
     }
-      
-  }
+
+}
+
+
+function preveriVnose(elementi, event) {
+
+    for (let i = 0; i < elementi.length; i++) {
+        if (!elementi[i].value.trim()) {
+            event.preventDefault();
+            alert('Vse oznacena polja so obvezna!');
+            return;
+        }
+
+    }
+}
+
+function maskaKlicniZnak(event) {
+    let kz = event.target.value.replace(/[^0-9A-Za-z]/g, ""); 
+
+    let st = kz.charAt(2).replace(/[a-zA-Z]/, ""); //stevilka
+    let crk = kz.slice(3, 6).replace(/[0-9]/,"").toUpperCase(); //crke
+
+    if (st && crk.length >= 1 && crk.length <= 3) {
+        event.target.value = "S5" + st + crk;
+    } else {
+        event.target.value = "S5" + st;
+    }
+}
